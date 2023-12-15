e73aac512bd9a426814518df6c87914e79d8d7ca
Jb <jbablestime@gmail.com>
2023-12-15 13:31:04 -0500

    Update installer.bat
    Cleaned it up, added new stuff

--- installer.bat
+++ installer.bat
@@ -1,16 +1,26 @@
 @echo off
 title  M-Terminal Installer
+echo Looking for older versions...
+del M-Terminal.exe
+timeout /nobreak /t 3 >nul
+cls
 echo Starting installer...
 py -m pip install pyserial
-py -m pipinstall colorama
+py -m pip install colorama
+py -m pip install pyinstaller
 echo Finished installing
+timeout /nobreak /t 3 >nul
+cls
 echo Starting build...
 pyinstaller --onefile main.py
+cls
 echo Cleaning up...
 del main.spec
 del main.py
 rmdir /s /q build
 move dist\main.exe M-Terminal.exe
 rmdir /s /q dist
+timeout /nobreak /t 3 >nul
+cls
 echo Finished building
 pause
