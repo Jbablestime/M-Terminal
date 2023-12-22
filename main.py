38ff84612bd22d33a337644070792a5ac6a2ef20
Jb <jbablestime@gmail.com>
2023-12-22 14:27:26 -0500

    Update main.py
    Added version 0.1.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import time
 import serial
 import serial.tools.list_ports
@@ -7,6 +8,11 @@ from os import system
 from colorama import *
 import requests
 import keyboard
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+import psutil
+import subprocess
 
 # Colors
 blue = Fore.BLUE
@@ -18,38 +24,21 @@ white = Fore.WHITE
 gray = Fore.LIGHTBLACK_EX
 orange = Fore.LIGHTYELLOW_EX
 
-# Settings
-developer_mode = True
-
-# Init
-system('title M-Terminal (Loading)')
-version = "1.0.1"
-
-def get_latest_release(repo_owner, repo_name):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        release_info = response.json()
-        return release_info['tag_name']
-    else:
-        print(f"Error: Unable to get release information. Status code: {response.status_code}")
-        return None
-
+ports = serial.tools.list_ports.comports()
 repo_owner = "justcallmekoko"
 repo_name = "ESP32Marauder"
-marauder_version = get_latest_release(repo_owner, repo_name)
-
-
-# Menus
-
-
+developer_mode = False
+version = "0.1.2"
+system(f"title M-Terminal │ Startup")
 
 def menu1():
     os.system('cls')
-    print(green + 
-f"""{yellow}
-attack
+    if ports:
+        system('title M-Terminal │ Connected')
+    if not ports:
+        system('title M-Terminal │ Not Connected')
+    try:
+        print(f"""{yellow}attack
 btspamall
 btwardrive
 channel
@@ -78,109 +67,174 @@ samsungblespam
 swiftpair
 update
 wardrive""")
-    input(f"{gray}Press {green}ENTER{gray} to continue")
-    main()
-
+        input(f"{gray}Press {green}ENTER{gray} to continue")
+        main()
+    except Exception:
+        print(f"{gray}[{yellow}System{gray}] {red}There was a issue loading the menu. If the issue persists, contact a developer.")
+        main()
 
 def menu2():
     os.system('cls')
-    ports = serial.tools.list_ports.comports()
-    if ports:
-        for port in ports:
-            print(f"{yellow}- {port.device}")
+    try:
+        ports = serial.tools.list_ports.comports()
+        if ports:
+            for port in ports:
+                print(f"{yellow}{port.device}")
+                input(f"{gray}Press {green}ENTER{gray} to continue")
+                main()
+        else:
+            print(yellow + "There are no active ports")
             input(f"{gray}Press {green}ENTER{gray} to continue")
             main()
-    else:
-        print(yellow + "There are no active ports")
-        input(f"{gray}Press {green}ENTER{gray} to continue")
+    except Exception:
+        print(f"{gray}[{yellow}System{gray}] {red}There was a issue loading the menu. If the issue persists, contact a developer.")
         main()
-            
 
 def connect_to_serial(port, baudrate=115200, timeout=1):
     try:
         return serial.Serial(port, baudrate, timeout=timeout)
     except serial.SerialException as e:
-        print(f"Error: {e}")
-        return None
-
-
-def serial_console(serial_connection):
-    print(f"{yellow}M-Terminal {gray}{version}{yellow} - Hold 'Spacebar' to exit")
-
-    accumulated_data = []
-    user_input = input("> ")
-
-    if user_input == "exit":
+        print(f"{gray}[{yellow}System{gray}] {red}Please ensure that your device is connected properly and that you have the proper drivers installed.")
+        time.sleep(10)
         main()
 
+def serial_console(serial_connection):
+    print(f"{yellow}M-Terminal {gray}{version}{yellow}")
+    print(red + "You must restart the program to exit console" + yellow)
+    accumulated_data = []
+    user_input = input("> ")
+    if user_input == "exit":
+        main()
     while True:     
         accumulated_data.append(user_input)
         serialized_data = "\n".join(accumulated_data) + "\n"
         serial_connection.write(serialized_data.encode())
         received_data = serial_connection.read_all().decode("utf-8")
         print(received_data, end="")
-        if keyboard.is_pressed("space"):
-            print(f"Ending loop")
-            break
         time.sleep(3)
+
 
 
 def menu3():
     os.system('cls')
-    print(yellow + "Please specify which port (e.g., COM1)" + white)
-    port_name = input("> ")
-
-    serial_connection = connect_to_serial(f"COM{port_name}")
-
-    if serial_connection:
-        try:
-            os.system('cls')
-            system(f'title M-Terminal (Active - COM{port_name})')
-            serial_console(serial_connection)
-        finally:
-            serial_connection.close()
-            main()
-    
+    try:
+        print(yellow + "Please specify which port (e.g., COM1)" + white)
+        port_name = input("> ")
+        serial_connection = connect_to_serial(f"COM{port_name}")
+        if serial_connection:
+            try:
+                os.system('cls')
+                system(f'title M-Terminal (Active - COM{port_name})')
+                serial_console(serial_connection)
+            except Exception:
+                main()
+            finally:
+                serial_connection.close()
+                main()
+    except Exception:
+        print(f"{gray}[{yellow}System{gray}] {red}There was a issue loading the menu. If the issue persists, contact a developer.")
+        main()
 
 def menu4():
-    os.system('cls')   
-    print(f"""
-{gray}[{yellow}Developer{gray}] {blue}jbablestime
+    os.system('cls')
+    try: 
+        print(f"""{gray}[{yellow}Developer{gray}] {blue}jbablestime
 {gray}[{yellow}Version{gray}] {green}{version}
-{gray}[{yellow}Marauder Version{gray}] {marauder_version}""")
-    input(f"{gray}Press {green}ENTER{gray} to continue")
-    main()
+{gray}[{yellow}Marauder Version{gray}] {release_info['tag_name']}""")
+        input(f"{gray}Press {green}ENTER{gray} to continue")
+        main()
+    except Exception:
+        print(f"{gray}[{yellow}System{gray}] {red}There was a issue loading the menu. If the issue persists, contact a developer.")
+        main()
+
+
+def menu5():
+    os.system('cls')
+    try:
+        print(f"{gray}[{yellow}System{gray}] {red}This feature is not ready yet!")
+        time.sleep(5)
+        main()
+    except Exception:
+        main()
+
+        
 
 
 def main():
-    system('title M-Terminal (Inactive)')
     os.system('cls')
-    print(f"{gray}[{yellow}1{gray}] {blue}Help{white}  {gray}[{yellow}2{gray}] {green}Ports{white}  {gray}[{yellow}3{gray}] {orange}Console{white}  {gray}[{yellow}4{gray}] {purple}Info{white}")
-    command = input(yellow + "> ")
+    if ports:
+        system('title M-Terminal │ Connected')
+    if not ports:
+        system('title M-Terminal │ Not Connected')
+    try:
+        print(f"{gray}[{yellow}1{gray}] {blue}Help{white}  {gray}[{yellow}2{gray}] {green}Ports{white}  {gray}[{yellow}3{gray}] {orange}Console{white}  {gray}[{yellow}4{gray}] {purple}Info{white}  {gray}[{yellow}5{gray}] {red}Upload{white}")
+        command = input(yellow + "> ")
 
-    if command == "1":
-        menu1()
-    elif command == "2":
-        menu2()
-    elif command == "3":
-        menu3()
-    elif command == "4":
-        menu4()
+        if command == "1":
+            menu1()
+        elif command == "2":
+            menu2()
+        elif command == "3":
+            menu3()
+        elif command == "4":
+            menu4()
+        elif command == "5":
+            menu5
+        elif command == "6":
+            os.system('cls')
+            print(f"{gray}[{yellow}System{gray}] {yellow}Restarting...")
+            time.sleep(2.5 )
+            os.system('cls')
+            python = sys.executable
+            subprocess.run([python] + sys.argv)
+        else:
+            os.system('cls')
+            print(f"{gray}[{yellow}System{gray}] {red}That page doesn't exist!")
+            time.sleep(2.5)
+            main()
+    except Exception:
+        system('title M-Terminal │ Error')
+        print(f"{gray}[{yellow}System{gray}] {red}There was a issue loading the menu. If the issue persists, contact a developer.")
 
-if __name__ == "__main__":
+
+try:
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+    response = requests.get(url)
+    print(f"{yellow}Welcome to {blue}M-Terminal{yellow}")
+    time.sleep(1.5)
+    os.system('cls')
+    print(f"{gray}[{yellow}System{gray}] {yellow}Starting...")
+    print(f"{gray}[{yellow}System{gray}] {yellow}Getting latest Marauder version...")
+    if response.status_code == 200:
+        release_info = response.json()
+        print(f"{gray}[{yellow}System{gray}] {yellow}The latest version is " + green + release_info['tag_name'])
+        time.sleep(2.5)
+        os.system('cls')
+    print(f"{gray}[{yellow}System{gray}] {yellow}Checking application files...")
     file_path = sys.argv[0]
-
     if file_path.lower().endswith(".py") and developer_mode == False:
-        print(f"{red}Error: {yellow}This script cannot be executed as a Python file. Please run the {green}installer.bat")
+        system('title M-Terminal │ Error')
+        print(f"{gray}[{yellow}System{gray}] {red}This file cannot be ran as a Python file, please run the {yellow}installer.bat{red}.")
         time.sleep(5)
         sys.exit()
-    elif file_path.lower().endswith(".exe") and developer_mode == False:
-        print("Starting...")
-        print(f"You're using {blue}M-Terminal {green}{version}" + white)
-        time.sleep(2)
-        main()
     elif file_path.lower().endswith(".py") and developer_mode == True:
-        print("Starting...")
-        print(f"You're using {blue}M-Terminal {green}{version}" + white)
+        print(f"{gray}[{yellow}System{gray}] {yellow}Bypassing application check, hello developer.")
+        time.sleep(2.5)
+        os.system('cls')
+        print(f"{gray}[{yellow}System{gray}] {green}Start complete, continuing...")
         time.sleep(2)
         main()
+    elif file_path.lower().endswith(".exe"):
+        os.system('cls')
+        print(f"{gray}[{yellow}System{gray}] {green}Start complete, continuing...")
+        time.sleep(2)
+        main()
+    elif response.status_code == 404:
+        system('title M-Terminal │ Error')
+        print(f"{gray}[{yellow}System{gray}] {red}There was a problem getting the latest version, check your internet connection and try again.")
+        pass
+except Exception:
+    system('title M-Terminal │ Error')
+    print(f"{gray}[{yellow}System{gray}] {red}There was a problem, check your internet connection and try again.")
+    os.system('pause')
+
